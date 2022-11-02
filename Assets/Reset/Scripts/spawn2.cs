d10457d74d2a6532492a3ae925e4b02d6f64669a
tommy-111213 <tomigashigashi.1213@gmail.com>
2022-11-02 12:17:45 +0900

    add reset

---
+++ Assets/Reset/Scripts/spawn2.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class spawn2 : MonoBehaviour
+{
+    [SerializeField]
+    GameObject objectPrefab;
+
+    public TrackableType type;
+
+    ARRaycastManager raycastManager;
+    List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (raycastManager.Raycast(touch.position, hitResults, TrackableType.PlaneWithinBounds))
+            {
+                Instantiate(objectPrefab, hitResults[0].pose.position, Quaternion.identity);
+            }
+        }
+    }
+}
