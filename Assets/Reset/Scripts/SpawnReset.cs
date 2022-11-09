cf3d46ef96e93ceec4613d0629b4a4905b2570d7
tommy-111213 <tomigashigashi.1213@gmail.com>
2022-11-09 14:49:39 +0900

    ResetButton Test.

--- Assets/Reset/Scripts/SpawnReset.cs
+++ Assets/Reset/Scripts/SpawnReset.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-public class spawn2 : MonoBehaviour
+public class SpawnReset : MonoBehaviour
 {
     [SerializeField]
     GameObject objectPrefab;
@@ -13,6 +13,9 @@ public class spawn2 : MonoBehaviour
 
     ARRaycastManager raycastManager;
     List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    private int furnitureNum = 0;
+    private string furnitureName = "furniture";
 
     // Start is called before the first frame update
     void Start()
@@ -29,8 +32,20 @@ public class spawn2 : MonoBehaviour
 
             if (raycastManager.Raycast(touch.position, hitResults, TrackableType.PlaneWithinBounds))
             {
-                Instantiate(objectPrefab, hitResults[0].pose.position, Quaternion.identity);
+                GameObject furniture = Instantiate(objectPrefab, hitResults[0].pose.position, Quaternion.identity);
+                furniture.name = furnitureName + furnitureNum.ToString("00000");
+                furnitureNum++;
             }
         }
     }
+
+    public void ResetAllObject()
+    {
+        while(furnitureNum > 0){
+            furnitureNum--;
+            GameObject obj = GameObject.Find(furnitureName + furnitureNum.ToString("00000"));
+            Destroy(obj);
+        }
+    }
+
 }
