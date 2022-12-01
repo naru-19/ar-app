a0853ef8e2b00f6e575ca7271e350836f4ceb234
aaaaa0114 <asadan0114@gmail.com>
2022-12-01 19:42:50 +0900

    add reset and resize function to spawndemo

--- Assets/SpawnDemo/Scripts/InputfieldSpawnManager.cs
+++ Assets/SpawnDemo/Scripts/InputfieldSpawnManager.cs
@@ -7,23 +7,23 @@ public class InputfieldSpawnManager : MonoBehaviour
 {
     //InputFieldを格納するための変数
     TMP_InputField inputField;
-    public spawn spawner;
- 
- 
+    public Spawn spawner;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //InputFieldコンポーネントを取得
         inputField = GameObject.Find("InputField (TMP)").GetComponent<TMP_InputField>();
     }
- 
- 
+
+
     //入力された名前情報を読み取ってコンソールに出力する関数
     public void SetText()
     {
         //InputFieldからテキスト情報を取得する
         spawner.load_id_object(inputField.text);
- 
+
         //入力フォームのテキストを空にする
         inputField.text = "";
     }
--- Assets/SpawnDemo/Scripts/spawn.cs
+++ Assets/SpawnDemo/Scripts/spawn.cs
@@ -2,17 +2,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-public class spawn : MonoBehaviour
+public class Spawn : MonoBehaviour
 {
+    [SerializeField]
+    GameObject Testprefab; // テスト用(白い家)
     ARRaycastManager raycastManager;
     BundleWebLoader loader;
 
     GameObject objectPrefab;
-
-    public delegate void Callback(Action<GameObject> gameObject);
 
     private void Awake()
     {
@@ -25,7 +26,8 @@ public class spawn : MonoBehaviour
     void Start()
     {
         loader = new BundleWebLoader();
-        StartCoroutine(loader.GetBundle((GameObject obj) => {
+        StartCoroutine(loader.GetBundle((GameObject obj) =>
+        {
             objectPrefab = obj;
         }, 0));
     }
@@ -43,87 +45,27 @@ public class spawn : MonoBehaviour
         if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
         {
             var hitPose = hits[0].pose;
-            // ダウンロードに成功していればインスタンス化
-            if(objectPrefab != null)
+            // 他にやることが無かったら
+            if (EventSystem.current.currentSelectedGameObject == null)
             {
+                // prefabのダウンロードが失敗していたら
+                if (objectPrefab == null)
+                {
+                    return;
+                }
                 Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
+                // Instantiate(Testprefab, hitPose.position, hitPose.rotation);
             }
         }
     }
 
     public void load_id_object(string id_input)
     {
+        // 入力文字をintに変更
         int id = Int32.Parse(id_input);
-        StartCoroutine(loader.GetBundle((GameObject obj) => {
+        StartCoroutine(loader.GetBundle((GameObject obj) =>
+        {
             objectPrefab = obj;
         }, id));
     }
-    // void Update()
-    // {   
-    //     Inputfield.GetInputid();
-    //     if (String.IsNullOrWhiteSpace(id_input))
-    //     {
-    //         if (Input.touchCount == 0 || Input.GetTouch(0).phase != TouchPhase.Ended)
-    //         {
-    //             return;
-    //         }
-
-    //         var hits = new List<ARRaycastHit>();
-    //         if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
-    //         {
-    //             var hitPose = hits[0].pose;
-    //             // ダウンロードに成功していればインスタンス化
-    //             if(objectPrefab != null)
-    //             {
-    //                 Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
-    //             }
-    //         }
-    //     }
-    //     else
-    //     {
-    //         int id = int.Parse(id_input);
-    //         if(id != prev_id)
-    //         {
-    //             prev_id = id;
-    //             Debug.Log(prev_id);
-    //             StartCoroutine(loader.GetBundle((GameObject obj) => {
-    //                 objectPrefab = obj;
-    //             }, prev_id));
-    //             if (Input.touchCount == 0 || Input.GetTouch(0).phase != TouchPhase.Ended)
-    //             {
-    //                 return;
-    //             }
-
-    //             var hits = new List<ARRaycastHit>();
-    //             if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
-    //             {
-    //                 var hitPose = hits[0].pose;
-    //                 // ダウンロードに成功していればインスタンス化
-    //                 if(objectPrefab != null)
-    //                 {
-    //                     Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
-    //                 }
-    //             }
-    //         }
-    //         else
-    //         {
-    //             if (Input.touchCount == 0 || Input.GetTouch(0).phase != TouchPhase.Ended)
-    //             {
-    //                 return;
-    //             }
-
-    //             var hits = new List<ARRaycastHit>();
-    //             if (raycastManager.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
-    //             {
-    //                 var hitPose = hits[0].pose;
-    //                 // ダウンロードに成功していればインスタンス化
-    //                 if(objectPrefab != null)
-    //                 {
-    //                     Instantiate(objectPrefab, hitPose.position, hitPose.rotation);
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    // }
 }
