a8292f73c981f81a44aa56aad6950537928aa669
naru-19 <narururu19@gmail.com>
2022-12-06 23:00:35 +0900

    add comment

--- Assets/Depth/Scripts/RenderDepthImage.cs
+++ Assets/Depth/Scripts/RenderDepthImage.cs
@@ -40,28 +40,18 @@ public class RenderDepthImage : MonoBehaviour
 
     private static void UpdateRawImage(RawImage rawImage, XRCpuImage cpuImage)
     {
-        // Get the texture associated with the UI.RawImage that we wish to display on screen.
-        var texture = rawImage.texture as Texture2D;  // cast
-
-        // If the texture hasn't yet been created, or if its dimensions have changed, (re)create the texture.
-        // Note: Although texture dimensions do not normally change frame-to-frame, they can change in response to
-        //    a change in the camera resolution (for camera images) or changes to the quality of the human depth
-        //    and human stencil buffers.
+        // depthを貼るtexture 
+        var texture = rawImage.texture as Texture2D;
         if (texture == null || texture.width != cpuImage.width || texture.height != cpuImage.height)
         {
             texture = new Texture2D(cpuImage.width, cpuImage.height, cpuImage.format.AsTextureFormat(), false);
             rawImage.texture = texture;
         }
 
-        // For display, we need to mirror about the vertical access.
+        // 生データはy軸でn反転しているのでレンダリング用に反転
         var conversionParams = new XRCpuImage.ConversionParams(cpuImage, cpuImage.format.AsTextureFormat(), XRCpuImage.Transformation.MirrorY);
-
-        //Debug.Log("Texture format: " + cpuImage.format.AsTextureFormat()); -> RFloat
-
-        // Get the Texture2D's underlying pixel buffer.
+        
         var rawTextureData = texture.GetRawTextureData<byte>();
-
-        // Make sure the destination buffer is large enough to hold the converted data (they should be the same size)
         Debug.Assert(
             rawTextureData.Length == cpuImage.GetConvertedDataSize(
                 conversionParams.outputDimensions, conversionParams.outputFormat
@@ -69,7 +59,7 @@ public class RenderDepthImage : MonoBehaviour
             "The Texture2D is not the same size as the converted data."
         );
 
-        // Perform the conversion.
+        // cpuImageをtextureにレンダリング
         cpuImage.Convert(conversionParams, rawTextureData);
         texture.Apply();
     }
