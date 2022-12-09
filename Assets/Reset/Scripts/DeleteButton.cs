42de8d0fbe63cbda45708681072080d9f3ce3fc6
aaaaa0114 <asadan0114@gmail.com>
2022-12-09 18:28:01 +0900

    delete an object2

--- Assets/Reset/Scripts/DeleteButton.cs
+++ Assets/Reset/Scripts/DeleteButton.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DeleteButton : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class DeleteButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void OnPushDeleteButton()
