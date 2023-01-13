4456e9173a41da55d4e9f74c14e445fd2e49f8da
kfukui <fukui.kaito@ist.osaka-u.ac.jp>
2023-01-13 15:30:22 +0900

    [add] 統合シーンの作成
    各々の作成したシーンを１つのシーンとして用いるために、統合した

--- Assets/toppage/scripts/MoveScene.cs
+++ Assets/toppage/scripts/MoveScene.cs
@@ -7,12 +7,12 @@ using UnityEngine.SceneManagement;
 public class MoveScene : MonoBehaviour
 {
     public SceneObject m_nextScene;
-    public void OnClick() //ARmodeへの遷移
+    public void OnClick() //シーンの遷移
     {
         Debug.Log(m_nextScene + "への遷移ボタンが押された!");
         StartCoroutine(DelayMoving(1, () =>
         {
-            // delay後の処理 ()内のシーンに遷移
+            // delay後の， ()内のシーンに遷移
             SceneManager.LoadScene(m_nextScene);
         }));
     }
